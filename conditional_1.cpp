aaebfea13cda91d68af3423c172d44c91c4cf7cc
Sohan Ahmed <157308345+sohanahmed123@users.noreply.github.com>
2024-09-16 12:08:32 +0600

    Add files via upload

---
+++ conditional_1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x=10;
+    if(x == 10)
+    {
+        cout << "It is ten\n";
+    }
+    else
+    {
+        cout << "It is not ten";
+    }
+    return 0;
+}
+
